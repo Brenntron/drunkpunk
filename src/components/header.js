a1e65a96ba0f8c3e7bae946931df3733476015a1
Brennan Willingham <brennan.willingham@gmail.com>
2020-06-28 11:22:44 -0500

    Add node-sass, redux, redux-saga, eslint, and prettier. Move css file to scss file. Add component directory.

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.scss";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__item">"OUR BREWS"</div>
+      <div className="header__item">"WHERE TO BUY"</div>
+      <div className="header__item">"CONTACT"</div>
+    </div>
+  );
+}
+
+export default Header;
